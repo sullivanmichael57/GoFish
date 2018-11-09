91582fdad11d920d9c92334a24127d281000a62a
kavyaduvedi <36945455+kavyaduvedi@users.noreply.github.com>
2018-11-08 20:36:50 -0600

    Add files via upload

--- go_fish.cpp
+++ go_fish.cpp
@@ -9,210 +9,150 @@
 using namespace std;
 
 
-
-// PROTOTYPES for functions used by this demonstration program:
-//void dealHand(Deck &d, Player &p, int numCards);
-
-
-//void bookPairs(Player p);
-/*
- * first: player books all pairs they have
- * loop {
- * player1 asks player 2 if they have a certain rank
- * if player 2 has any cards of that rank, they are given to player 1
- * if not, player 1 must go fish
- * player 1 checks for pairs again and books them
- * player 2 repeats
- * }
- *
- */
-
-
-
-
-
-
 int main(){
     ofstream myfile;
     myfile.open("outputfile.txt"); 
-    //myfile << "Hello World" << endl;
 
-    Player p1 = Player("Michael");
-    Player p2 = Player("Kavya");  
+    //players and pointers to players are created 
+    Player pl1 = Player("Michael");
+    Player pl2 = Player("Kavya");  
+    Player *p1 = &pl1; 
+    Player *p2 = &pl2; 
     Deck d;
     d.shuffle();
 
-    for (int i = 0;i<7;i++){ // deal cards to players
-        p1.addCard(d.dealCard());
-        p2.addCard(d.dealCard());
+    //deals cards to players
+    for (int i = 0;i<7;i++){ 
+        p1->addCard(d.dealCard());
+        p2->addCard(d.dealCard());
     }
 
+    //shows original hands prior to booking cards
+    myfile << p1->getName() <<"'s original hand" << ": " << p1->showHand() << endl;
+    myfile << p2->getName() <<"'s original hand" << ": " << p2->showHand() << endl;
+    myfile << " " << endl;  
 
-    //myfile << p1.getName() << ": " << p1.showHand() << endl;
-    //myfile << p2.getName() << ": " << p2.showHand() << endl;
+    //books cards for both players 
+    Card c1 = Card(); 
+    Card c2 = Card(); 
+    while(p1->checkHandForPair(c1,c2))
+    {
+        p1->checkHandForPair(c1,c2); 
+        p1->bookCards(c1,c2);     
+        p1->removeCardFromHand(c1);
+        p1->removeCardFromHand(c2);
+    }
 
-    //bookPairs(p1);
-    //bookPairs(p2);
-    //myfile << p1.getName() << ": " << p1.showBooks() << endl;
-    //myfile << p2.getName() << ": " << p2.showBooks() << endl;
-
+    while(p2->checkHandForPair(c1,c2))
+    {
+        p2->checkHandForPair(c1,c2); 
+        p2->bookCards(c1,c2);     
+        p2->removeCardFromHand(c1);
+        p2->removeCardFromHand(c2);
+    }
 
     //This while loop is the big boy, when it is over, the game is over
-  
-    int turn = 0; 
-    while(p1.getBookSize() + p2.getBookSize() < 52 && d.size()>0){
-        myfile << p1.getName() << ": " << p1.showHand() << endl;
-        myfile << p2.getName() << ": " << p2.showHand() << endl;
-        if(p1.getHandSize() == 0)
+    while(p1->getBookSize() + p2->getBookSize() < 52) {
+        myfile << p1->getName() << ": " << p1->showHand() << endl;
+        myfile << p2->getName() << ": " << p2->showHand() << endl;
+
+        //if player 1 has no cards in hand they draw a card 
+        if(p1->getHandSize() == 0)
         {
-             p1.addCard(d.dealCard());
+             p1->addCard(d.dealCard());
 
         }
+
         else
         {
-            Card card_chosen = p1.chooseCardFromHand();
-            myfile << p1.getName() <<" asks- Do you have a " << card_chosen.rankString(card_chosen.getRank()) << "?" << endl;
+            Card card_chosen = p1->chooseCardFromHand();
+            myfile << p1->getName() <<" asks- Do you have a " << card_chosen.rankString(card_chosen.getRank()) << "?" << endl;
 
-            if (p2.sameRankInHand(card_chosen)){
-                //okay they had at least one of the cards, remove them from p2 and give to p1
+            if (p2->sameRankInHand(card_chosen)){
+                //player 2 has at least one card of the rank that player 1 is asking about 
+
                 int count = 0; 
-                if (p2.cardInHand(Card(card_chosen.getRank(),Card::diamonds))){
-                    p1.addCard(p2.removeCardFromHand(Card(card_chosen.getRank(),Card::diamonds)));
+                if (p2->cardInHand(Card(card_chosen.getRank(),Card::diamonds))){
+                    p1->addCard(p2->removeCardFromHand(Card(card_chosen.getRank(),Card::diamonds)));
                     count++; 
 
                 }
-                else if (p2.cardInHand(Card(card_chosen.getRank(),Card::hearts))){
+                else if (p2->cardInHand(Card(card_chosen.getRank(),Card::hearts))){
 
-                    p1.addCard(p2.removeCardFromHand(Card(card_chosen.getRank(),Card::hearts)));
+                    p1->addCard(p2->removeCardFromHand(Card(card_chosen.getRank(),Card::hearts)));
                     count++; 
 
                 }
-                else if (p2.cardInHand(Card(card_chosen.getRank(),Card::clubs))){
-                    p1.addCard(p2.removeCardFromHand(Card(card_chosen.getRank(),Card::clubs)));
+                else if (p2->cardInHand(Card(card_chosen.getRank(),Card::clubs))){
+                    p1->addCard(p2->removeCardFromHand(Card(card_chosen.getRank(),Card::clubs)));
                     count++; 
 
                 }
-                else if (p2.cardInHand(Card(card_chosen.getRank(),Card::spades))){
-                    p1.addCard(p2.removeCardFromHand(Card(card_chosen.getRank(),Card::spades)));
+                else if (p2->cardInHand(Card(card_chosen.getRank(),Card::spades))){
+                    p1->addCard(p2->removeCardFromHand(Card(card_chosen.getRank(),Card::spades)));
                     count++; 
 
                 }
-                myfile << p2.getName() <<" says- Yes. I have " << count << " " << " cards of rank " << card_chosen.rankString(card_chosen.getRank()) <<"." << endl;
+                myfile << p2->getName() <<" says- Yes. I have " << count << " " << "cards of rank " << card_chosen.rankString(card_chosen.getRank()) <<"." << endl;
             }
 
             else
             {
-                myfile << p2.getName() <<" says- Go Fish." << endl;
+                //player 2 does not have any cards of that rank so player 1 draws a card from the deck 
+                myfile << p2->getName() <<" says- Go Fish." << endl;
                 Card drawn = d.dealCard(); 
-                myfile <<p1.getName() << " has drawn a " << drawn.toString() << endl;   
-                p1.addCard(drawn);
+                myfile <<p1->getName() << " has drawn a " << drawn.toString() << endl;   
+                p1->addCard(drawn);
             }
-       
+
+            
+            //player 1 checks for books and books any pairs 
             Card c1 = Card(); 
             Card c2 = Card(); 
-            while(p1.checkHandForPair(c1,c2))
+            while(p1->checkHandForPair(c1,c2))
             {
-            p1.checkHandForPair(c1,c2); 
-            p1.bookCards(c1,c2);     
-            p1.removeCardFromHand(c1);
-            p1.removeCardFromHand(c2);
-            }
-     
+                p1->checkHandForPair(c1,c2); 
+                p1->bookCards(c1,c2);     
+                p1->removeCardFromHand(c1);
+                p1->removeCardFromHand(c2);
+            }   
             
         }
-        myfile << p1.getName() << " has " << p1.getBookSize()/2 << " books." << endl;
-        myfile << p1.getName() << "'s books: " << p1.showBooks() << endl;
-        //myfile << p1.getName() << ": " << p1.showHand() << endl;
-        //myfile << p2.getName() << ": " << p2.showHand() << endl;
-         
 
-        //change turns
-        if(d.size() != 0) 
-        {    
-            myfile << "Next round" << endl;
-            myfile << p1.getName() << ": " << p1.showHand() << endl;
-            myfile << p2.getName() << ": " << p2.showHand() << endl;
-            if(p2.getHandSize() == 0)
-            {
-                 p2.addCard(d.dealCard());
+        //shows player 1's books and how many books player 1 has
+        myfile << p1->getName() << " has " << p1->getBookSize()/2 << " books." << endl;
+        myfile << p1->getName() << "'s books: " << p1->showBooks() << endl;
 
-            }
-            else
-            {
-                Card card_chosen = p2.chooseCardFromHand();
-                myfile << p2.getName() <<" asks- Do you have a " << card_chosen.rankString(card_chosen.getRank()) << "?" << endl;
+        //pointers to player switch so player 1 becomes player 2
+        //i.e. the player who is asking for a certain card becomes the player who is asked (and vice versa)
+        Player *temp = p1; 
+        p1 = p2; 
+        p2 = temp; 
+        myfile << " " << endl;
 
-                if (p1.sameRankInHand(card_chosen)){
-                    //okay they had at least one of the cards, remove them from p1 and give to p2
-                    int count = 0; 
-                    if (p1.cardInHand(Card(card_chosen.getRank(),Card::diamonds))){
-                        p2.addCard(p1.removeCardFromHand(Card(card_chosen.getRank(),Card::diamonds)));
-                        count++; 
-
-                    }
-                    else if (p1.cardInHand(Card(card_chosen.getRank(),Card::hearts))){
-
-                        p2.addCard(p1.removeCardFromHand(Card(card_chosen.getRank(),Card::hearts)));
-                        count++; 
-
-                    }
-                    else if (p1.cardInHand(Card(card_chosen.getRank(),Card::clubs))){
-                        p2.addCard(p1.removeCardFromHand(Card(card_chosen.getRank(),Card::clubs)));
-                        count++; 
-
-                    }
-                    else if (p1.cardInHand(Card(card_chosen.getRank(),Card::spades))){
-                        p2.addCard(p1.removeCardFromHand(Card(card_chosen.getRank(),Card::spades)));
-                        count++; 
-
-                    }
-                    myfile << p1.getName() <<" says- Yes. I have " << count << " " << " cards of rank " << card_chosen.rankString(card_chosen.getRank()) <<"." << endl;
-                }
-
-                else
-                {
-                    myfile << p1.getName() <<" says- Go Fish." << endl;  
-                    Card drawn2 = d.dealCard(); 
-                    myfile << p2.getName() << " has drawn a " << drawn2.toString() << endl; 
-                    p2.addCard(drawn2);
-                }
-           
-                Card c1 = Card(); 
-                Card c2 = Card(); 
-                while(p2.checkHandForPair(c1,c2))
-                {
-                p2.checkHandForPair(c1,c2); 
-                p2.bookCards(c1,c2);     
-                p2.removeCardFromHand(c1);
-                p2.removeCardFromHand(c2);
-                }
-         
-                
-            }
-            myfile << p2.getName() << " has " << p2.getBookSize()/2 << " books." << endl;
-            myfile << p2.getName() << "'s books: " << p2.showBooks() << endl;
-            //myfile << p2.getName() << ": " << p2.showHand() << endl;
-            //myfile << p1.getName() << ": " << p1.showHand() << endl;
-            myfile << "Next round" << endl;
-        }
-
-  
+        if(p1->getBookSize() + p2->getBookSize() < 52)
+            myfile <<"Next Round" << endl;        
 
     }
-    myfile << p1.getName() << " has " << p1.getBookSize()/2 << " books." << endl;
-    myfile << p2.getName() << " has " << p2.getBookSize()/2 << " books." << endl;
-    if(p1.getBookSize() > p2.getBookSize())
+
+    //displays the winner depending on who has more books 
+    myfile << "Game Over!" << endl; 
+    myfile << p1->getName() << " has " << p1->getBookSize()/2 << " books." << endl;
+    myfile << p2->getName() << " has " << p2->getBookSize()/2 << " books." << endl;
+
+    if(p1->getBookSize() > p2->getBookSize())
     {
-        myfile << p1.getName() << " wins!!!!!" << endl; 
+        myfile << p1->getName() << " wins!!!!!" << endl; 
     }
-    else if(p1.getBookSize() < p2.getBookSize())
+    else if(p1->getBookSize() < p2->getBookSize())
     {
-        myfile << p2.getName() << " wins!!!!!" << endl; 
+        myfile << p2->getName() << " wins!!!!!" << endl; 
     }
     else
     {
         myfile << "There is a tie!" << endl; 
     }
+
     myfile << "The Game Has Ended. Wasn't That Fun!" << endl;
     myfile.close(); 
     return 0;
